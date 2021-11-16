d45acd4cfaf8ee5e225a3bd03864246a05b6ba1c
aleraich <alejandro.carreno.huaipara@gmail.com>
2021-11-16 03:17:34 -0300

    administrador

--- resources/views/auth/register2.blade.php
+++ resources/views/auth/register2.blade.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Admin</title>
-    <link rel="stylesheet" href="{{ asset('bootstrap-3.1.1/css/bootstrap.min.css') }}">
+    <link rel="stylesheet"  href="{{ asset('bootstrap@5.1.3/dist/css/bootstrap.min.css') }}">
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -18,16 +18,17 @@
     <link rel="stylesheet" href="../CSS/cabecera.css"> 
     <script src="../CSS/script.js" defer></script>
     <div class="d-sm-none d-md-block d-none d-lg-block cabeceraCss"> 
-        <div class="cabeceraCssAzul"></div>
-        <div class="cabeceraCssAzulClaro"></div>
-        <div class="cabeceraCssRoja"></div>
-        <div class="cabeceraCssRojoClaro"></div>
-        <div class="cabeceraCssBlanca"></div>
-        <div class="textoCabecera h3">INFORMATICA - SISTEMAS</div>
-        <div class="textoCabeceraUniverisdad h3">UNIVERSIDAD MAYOR DE SAN SIMON</div>
-        <img class="logoUmssCss" src="../IMAGENES/LogoUMSS.png" alt="">
-        <img class="logoCarreraCss" src="../IMAGENES/logoInformaticaSistemas.png" alt="">
-    </div>
+    <div class="cabeceraCssAzul"></div>
+    <div class="cabeceraCssAzulClaro"></div>
+    <!-- <div class="cabeceraCssRoja"></div> -->
+    <!-- <div class="cabeceraCssRojoClaro"></div> -->
+    <div class="cabeceraCssBlanca"></div>
+    <div class="textoCabecera h3">UNIVERSIDAD MAYOR DE SAN SIMON</div>
+    <div class="textoCabeceraUniverisdad h3">FACULTAD DE CIENCIAS Y TECNOLOGIA</div>
+    <img class="logoUmssCss" src="IMAGENES/LogoUMSS.png" alt="">
+    <!-- <img class="logoCarreraCss" src="IMAGENES/logoInformaticaSistemas.png" alt="">
+ -->
+  </div>
 </head>
 <header>
     
@@ -44,7 +45,7 @@
             </a>
             <div class="navbar-links">
               <ul>
-                <li><a href="{{ url('/docente/dashboard') }}">Inicio</a></li>
+                <li><a href="{{ url('/admin/dashboard') }}">Inicio</a></li>
                 <li><a href="{{ route('auth.register2') }}">Registrar docentes</a></li>
                 <li><a href="{{ route('auth.logout') }}">Cerrar sesion</a></li>
                 
@@ -83,7 +84,8 @@
                  <input type="password" class="form-control" name="password" placeholder="Ingrese una contraseña">
                  <span class="text-danger">@error('password'){{ $message }} @enderror</span>
               </div>
-              <button type="submit" class="btn btn-block btn-primary">Registrar</button>
+              
+              <button Style="margin-bottom: 120px; background-color: #215f88; margin-top: 50px; " type="submit" class="btn btn-primary" >Registrar</button>
               <br>              
            </form>
       </div>
