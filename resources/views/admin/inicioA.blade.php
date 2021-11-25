ba7b2ae350255b272c7723c46bbee5713accdf34
aleraich <alejandro.carreno.huaipara@gmail.com>
2021-11-25 02:07:19 -0300

    listo todo funcionando

--- resources/views/admin/inicioA.blade.php
+++ resources/views/admin/inicioA.blade.php
@@ -10,6 +10,7 @@
             <div class="navbar-links">
               <ul>
                 <li><a href="{{ route('admin.inicioA') }}">Inicio</a></li>
+                <li><a href="{{ url('/admin/lista') }}">Lista de empresas</a></li>
                 <li><a href="{{ route('auth.register2') }}">Registrar docentes</a></li>
                 <li><a href="{{ route('auth.logout') }}">Cerrar sesion</a></li>
             
--- resources/views/auth/register2.blade.php
+++ resources/views/auth/register2.blade.php
@@ -10,10 +10,6 @@
             <div class="navbar-links">
               <ul>
                 <li><a href="{{ route('admin.inicioA') }}">Inicio</a></li>
-<<<<<<< HEAD
-=======
-                <li><a href="{{ route('estudiante.documentosB') }}">Documentos base</a></li>
->>>>>>> AleRaiCH
                 <li><a href="{{ url('/admin/lista') }}">Lista de empresas</a></li>
                 <li><a href="{{ route('auth.register2') }}">Registrar docentes</a></li>
                 <li><a href="{{ route('auth.logout') }}">Cerrar sesion</a></li>
