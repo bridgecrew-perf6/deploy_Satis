3dc96dd93c46d0bbf9bc4c991a0e42762e8a82ed
aleraich <alejandro.carreno.huaipara@gmail.com>
2021-12-28 01:18:48 -0300

    estilos pagos y trabajo

--- resources/views/planTrabajos/show.blade.php
+++ resources/views/planTrabajos/show.blade.php
@@ -1,5 +1,39 @@
-@extends('layouts.plantilla')
-
+@extends('layouts.plantillaD')
+@section('content')
+          <nav class="navbar " >
+            
+            <div class="brand-title">TALLER DE INGENIERIA DE SOFTWARE</div>
+            <a href="#" class="toggle-button">
+              <span class="bar"></span>
+              <span class="bar"></span>
+              <span class="bar"></span>
+            </a>
+            <div class="navbar-links">
+              
+              <ul>
+                <li><a href="{{ route('estudiante.inicioE') }}">Inicio</a></li>
+                <li><a href="{{ route('estudiante.empresa') }}">Empresa</a></li>
+                <li><a href="{{ route('estudiante.documentosB') }}">Documentos base</a></li>
+                <li><a href="{{ url('/estudiante/lista') }}">Lista de empresas</a></li>
+                
+             <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Registrar
+          </a>
+          <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+            <li><a class="dropdown-item" href="{{ route('fundaempresa') }}">Registrar funda empresa TIS</a></li>
+            <li><a class="dropdown-item" href="{{ route('pagos.create') }}">Registrar plan de pagos</a></li>
+            <li><a class="dropdown-item" href="{{ route('planTrabajos.index') }}">Registrar plan de Trabajo</a></li>
+           
+          </ul>
+        </li>
+                <li><a href="{{ route('auth.logout') }}">Cerrar sesion</a></li>
+            
+          </ul>
+        </div>
+      </div>
+      </nav>
+      @endsection
 @section('cuerpo')
     <div class="row">
         <div class="col-lg-12 margin-tb">
@@ -12,7 +46,7 @@
             </div>
         </div>
     </div>
-
+    <div class="formPlanT">
     <div class="row">
         <div class="col-xs-12 col-sm-12 col-md-12">
             <div class="form-group">
@@ -52,4 +86,5 @@
             </div>
         </div>-->
     </div>
+</div>
 @endsection
