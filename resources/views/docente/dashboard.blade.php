21bff4b6d5338d220f0cd015e67b8e5d61e1b5c6
Flores Noemi <floresaserico.n@gmail.com>
2022-01-06 09:43:23 -0400

    se quito ver plan de pagos y plan de trabajo

--- resources/views/docente/dashboard.blade.php
+++ resources/views/docente/dashboard.blade.php
@@ -13,7 +13,7 @@
                 <li><a href="{{ route('docente.convocatoriasD') }}">Agregar convocatoria</a></li>
                 <li><a href="{{ route('docente.avisosD') }}">Agregar Avisos</a></li>
                 <li><a href="{{ url('/docente/lista') }}">Lista de empresas</a></li>
-                <li class="nav-item dropdown">
+                <!--<li class="nav-item dropdown">
                   <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                     ver
                   </a>
@@ -22,7 +22,7 @@
                     <li><a class="dropdown-item" href="{{ route('docente.planT') }}">Plan de Trabajo</a></li>
                    
                   </ul>
-                </li>
+                </li>-->
                 <li><a href="{{ url('/docente/calendario') }}">Calendario</a></li>
                 <li><a href="{{ route('auth.register') }}">Registrar estudiantes</a></li>
                 <li><a href="{{ route('auth.logout') }}">Cerrar sesion</a></li> 
